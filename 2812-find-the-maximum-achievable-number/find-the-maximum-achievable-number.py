ea7ee6ca1c6a7392a44dc3cf7d1cdb17d9af243b
Skavi <vdvmsender@gmail.com>
2025-12-09 10:22:08 +0100

    Time: 3 ms (17.54%) | Memory: 17.9 MB (31.73%) - LeetSync

---
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        x=0
+
+        for i in range(t):
+            
+            x=num+2*t
+        return x
+
+        
